9d61555af6457b9990677491318cfa24c4593ef5
paulo20341 <pauloeduardo200005@gmail.com>
2025-02-28 22:51:30 -0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  server: {
+    host: '0.0.0.0', // Permite conexões externas
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173, // Usa a porta da Render
   },
 });
