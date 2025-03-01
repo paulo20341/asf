62b7587776d4a6e8bacae293d3b13019cd9a6d4b
paulo20341 <pauloeduardo200005@gmail.com>
2025-02-28 23:00:45 -0300

    teste2

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './', // Garante que os arquivos estáticos sejam carregados corretamente
   server: {
-    host: '0.0.0.0', // Permite conexões externas
-    port: process.env.PORT ? parseInt(process.env.PORT) : 5173, // Usa a porta da Render
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
   },
 });
